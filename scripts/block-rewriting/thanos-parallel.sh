22c9571cffadc4294aaa9585f2d901f174c61bb9
Owen Williams <owen.williams@grafana.com>
2023-11-02 13:38:16 -0400

    use 16 processes

--- scripts/block-rewriting/thanos-parallel.sh
+++ scripts/block-rewriting/thanos-parallel.sh
@@ -14,4 +14,4 @@ rundir=$(dirname $0)
 blocks=$(ls -1 $workdir | grep 01 | sort -R)
 
 # Split the list into chunks of 10 and run 8 instances of thanos at a time.
-echo $blocks | xargs -P 8 -n 10 $rundir/block-rewrite.sh $workdir $tmpparent $todelete
+echo $blocks | xargs -P 16 -n 10 $rundir/block-rewrite.sh $workdir $tmpparent $todelete
